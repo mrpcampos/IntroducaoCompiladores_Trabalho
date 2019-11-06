15ebfa659f70fcc7d08499735225645fe2e8ed96
g-saldanha <gabriel@gbldev.com>
2019-11-06 19:15:02 -0300

    arrumando teste xor e nor

--- cap05/parser/langX.java
+++ cap05/parser/langX.java
@@ -61,9 +61,9 @@ public class langX implements langXConstants {
         int k;
         String s;
         s = tokenImage[x];
-        k = s.lastIndexOf("\u005c"");
+//        k = s.lastIndexOf("\u005c"");
         try {
-            s = s.substring(1,k);
+            s = s.substring(1,s.length()-1);
         } catch (StringIndexOutOfBoundsException e) {
             e.printStackTrace();
         }
